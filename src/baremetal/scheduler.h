76f6c0857220644ec9115c480bd170f7543307c2
Huseyin Cavus <huseyincavus@proton.me>
2026-07-26 17:57:47 +0300

    test: full single-layer G2 training graph test and scheduler wiring

--- src/baremetal/scheduler.h
+++ src/baremetal/scheduler.h
@@ -10,6 +10,7 @@ typedef struct bmt_scheduler_s bmt_scheduler_t;
 bmt_scheduler_t* bmt_scheduler_create(backend_ctx_t* backend, bmk_registry_t* reg, bmt_graph_t* graph, int max_seq_len, int kv_dim, int n_layers);
 void bmt_scheduler_destroy(bmt_scheduler_t* sched);
 void bmt_scheduler_run(bmt_scheduler_t* sched, int pos, int seq_len);
+void bmt_scheduler_forward_train(bmt_scheduler_t* sched, int S);
 
 /* Backward walk: reverse topological order, dispatch each node's backward
  * kernel using the forward activations (in buffers[]) + gradient buffers.
