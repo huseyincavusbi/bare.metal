fbaafb27112eab06568ed58812550ca4ad12ca15
Huseyin Cavus <huseyincavus@proton.me>
2026-07-29 17:50:45 +0300

    feat: bf16/fp16 matmul inference with weight conversion

--- src/baremetal/scheduler.h
+++ src/baremetal/scheduler.h
@@ -8,6 +8,7 @@
 typedef struct bmt_scheduler_s bmt_scheduler_t;
 
 bmt_scheduler_t* bmt_scheduler_create(backend_ctx_t* backend, bmk_registry_t* reg, bmt_graph_t* graph, int max_seq_len, int kv_dim, int n_layers);
+void bmt_scheduler_set_precision(bmt_scheduler_t* sched, bm_precision_t precision);
 void bmt_scheduler_destroy(bmt_scheduler_t* sched);
 void bmt_scheduler_run(bmt_scheduler_t* sched, int pos, int seq_len);
 void bmt_scheduler_forward_train(bmt_scheduler_t* sched, int S);
